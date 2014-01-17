f73b41c2d9e93f66deff3252c6fc012420c87750
IV1TR0 <Xandi98@web.de>
2014-01-17 21:22:22 +0100

    Alpha 1.1
    - created map v2
    - added index (main page)

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Startseite - Bromons</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <img src="images/pokeMonMap2.png" width="600" height="400" alt="theMap" usemap="#map">
+        <map name="map">
+            <area shape="poly" coords="237,113,121,178,116,300,400,305,404,173,240,114" href="/" alt="home" title="MyHome">
+        </map>
+    </body>
+</html>
