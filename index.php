3791840fd210e77a15d190e0bc423ffabc6e957d
IV1TR0 <Xandi98@web.de>
2014-02-01 14:58:37 +0100

    Alpha 1.3.1
    - Improved PopUp-Logic
    - improved style

--- index.php
+++ index.php
@@ -20,48 +20,21 @@ $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
             <area shape="rect" coords="223,140,369,254" href="#square" alt="square" title="Bromons Square">
         </map>
 
-        <div id="home" class="popUps">
-            <a href="index.php">⬅ Zurueck</a>
-            <div id="popUpBody">
-                <h2>Mein Zuhause</h2>
-            </div>
-        </div>
-
-        <div id="wilderness" class="popUps">
-            <a href="index.php">Zur&uuml;ck</a>
-            <div id="popUpBody">
-                <h2>Die Wildniss</h2>
-            </div>
-        </div>
-
-        <div id="shop" class="popUps">
-            <a href="index.php">Zur&uuml;ck</a>
-            <div id="popUpBody">
-                <h2>Der Shop</h2>
-            </div>
-        </div>
-
-        <div id="hospital" class="popUps">
-            <a href="index.php">Zur&uuml;ck</a>
-            <div id="popUpBody">
-                <h2>Das Krankenhaus</h2>
-            </div>
-        </div>
-
-        <div id="arena" class="popUps">
-            <a href="index.php">Zur&uuml;ck</a>
-            <div id="popUpBody">
-                <h2>Die Arena</h2>
-            </div>
-        </div>
-
-        <div id="square" class="popUps">
-            <a href="index.php">Zur&uuml;ck</a>
-            <div id="popUpBody">
-                <h2>Der Martkplatz</h2>
-            </div>
-
-        </div>
+        <?php
+            foreach ($sites as $site):
+        ?>
+            <div id="<?= $site[0] ?>" class="popUps">
+                <header>
+                    <a href="index.php">⬅ Zur&uuml;ck</a>
+                    <h2><?= $site[1] ?></h2>
+                </header>
+                <article>
+                    <?php include "sites/" . $site[0] . ".php"; ?>
+                </article>
+            </div>  
+        <?php
+            endforeach;
+        ?>
 
     </body>
 </html>
